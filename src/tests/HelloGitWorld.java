d3324121b1b3ca59a2760154eefbc084efd3a747
hiltas73 <h_iltas@yahoo.com>
2020-05-06 18:25:36 +0300

    update Spring.java

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -4,6 +4,7 @@ public class HelloGitWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello new world again");
+
     }
 
 }
---
+++ src/tests/Spring.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class Spring {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Again");
+        System.out.println("Hello Again one more time");
+        System.out.println("The last Hello");
+
+    }
+}
