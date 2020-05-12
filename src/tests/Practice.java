208692e4078b07dc34b0f0b2e70dfbc233ce3276
hiltas73 <h_iltas@yahoo.com>
2020-05-12 17:47:07 +0300

    update 2nd Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,6 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("1st version");
+        System.out.println("2nd version");
     }
 }
