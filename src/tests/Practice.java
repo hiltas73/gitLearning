ef33d340d1720d2236e30f63529cc1ed6815228e
hiltas73 <h_iltas@yahoo.com>
2020-05-12 17:52:09 +0300

    update 3rd Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("1st version");
         System.out.println("2nd version");
+        System.out.println("3rd version");
     }
 }
